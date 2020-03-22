8235a7f7dc958d98814cea5906632acfd7c66653
CIWang6636 <tim6636like@hotmail.com>
2020-03-23 01:04:04 +0800

    This is the final assignment for Week 1

---
+++ Week1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is the notebook used for Week 1 final assignment.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
